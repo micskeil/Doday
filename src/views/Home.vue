d2fc57aeb296ac2941306376fc3f7b4ecd2dfd98
micskeil <micskeil@gmail.com>
2021-01-21 21:52:05 +0100

    create NavMenu.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid ">
-    <div class="header container d-flex justify-content-between p-0">
+    <div class="header container d-flex justify-content-between p-0 pt-3">
       <CurrentDate />
       <div class="d-flex align-items-center pr-3" @click="toggleMenu">
         MENU
@@ -10,9 +10,7 @@
       v-if="isNavActive"
       class="nav container d-flex flex-column align-content-end"
     >
-      <div>
-        Logout
-      </div>
+      <NavMenu />
     </div>
   </div>
 </template>
@@ -20,11 +18,13 @@
 <script>
 // @ is an alias to /src
 import CurrentDate from "@/components/LayoutElements/CurrentDate.vue";
+import NavMenu from "../components/NavigationMenu/NavMenu.vue";
 
 export default {
   name: "Home",
   components: {
     CurrentDate,
+    NavMenu,
   },
   data() {
     return {
